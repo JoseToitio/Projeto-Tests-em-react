7b09b62c3b1a35d3aa72cb50d79b42862ff6f964
Jose <josetoitio@gmail.com>
2021-11-30 17:25:26 -0300

    requisito 4 completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <NotFount.js />', () => {
+  it('Teste se página contém um heading h2 com o texto ', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pageoff');
+    const heading = screen.getByRole('heading',
+      { name: /page requested not found/i, level: 2 });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem ', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pageoff');
+    const image = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
