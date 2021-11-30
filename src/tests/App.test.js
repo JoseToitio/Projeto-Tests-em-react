6edc5e974e935d33c5845330b73e0e88ba21f666
Jose <josetoitio@gmail.com>
2021-11-30 16:03:30 -0300

    Requisito 1 completo

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste no componente <App.js />', () => {
+  it('Deve possuir 3 links Home, About, Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    const linkFavorites = screen.getByRole('link', { name: /favorite pokémons/i });
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavorites).toBeInTheDocument();
+  });
+
+  it('Deve redirecionar para a pagina clicada', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/');
+    expect(history.location.pathname).toBe('/');
+    history.push('/about');
+    expect(history.location.pathname).toBe('/about');
+    history.push('/favorites');
+    expect(history.location.pathname).toBe('/favorites');
+  });
+});
